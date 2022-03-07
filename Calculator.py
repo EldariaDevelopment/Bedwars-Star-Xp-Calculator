e6f023e2e3189ecf1c4bcb5c5048df2c6778d8df
Almond <93007463+EldariaDevelopment@users.noreply.github.com>
2022-03-07 12:34:36 -0800

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+import time
+def S(S):
+  print(S)
+time = int(input("Input Loop Amount ")
+while True:
+  if time > 0:
+    #I'll Finish it Later
+      S('level', Level, 'Xp', Xp)
+    time = time-1
+#I hate this
+S(Sus)
